6293152389c1ee20d7e0bd1e64b2a79766488606
zhanglong <zhanglong@zhanglongdeMacBook-Pro.local>
2020-05-11 13:54:24 +0800

    feat(个人中心): 个人中心页面开发

--- src/pages/Person/person.jsx
+++ src/pages/Person/person.jsx
@@ -1,18 +1,70 @@
 import React from 'react'
-import { NavBar, Icon } from 'antd-mobile';
+import './poson.scss'
+import { NavBar, Icon, List } from 'antd-mobile';
+import person_icon from '../../assest/img/person_icon.png'
+import shoucang_icon from '../../assest/img/shoucang.png'
+import order_icon from '../../assest/img/dingdan.png'
+import parking_icon from '../../assest/img/tingche.png'
+import youhuquan_icon from '../../assest/img/youhuiquan.png'
+import set_icon from '../../assest/img/set_icon.png'
+import suggest_icon from '../../assest/img/suggest_icon.png'
+const Item = List.Item;
 export default class Person extends React.Component{
-    render () {
-        return (
-            <div>
-                <NavBar
-                mode="light"
-                icon={<Icon type="left" />}
-                onLeftClick={() => console.log('onLeftClick')}
-                >我的</NavBar>
-                <div>
-                    我的
-                </div>
-            </div>
-        )
-    }
+	render () {
+		return (
+			<div>
+				<NavBar
+				mode="light"
+				icon={<Icon type="left" />}
+				onLeftClick={() => console.log('onLeftClick')}
+				>我的</NavBar>
+				<div className="person-container">
+					<div className="person-top">
+						<img src={person_icon} alt=""/>
+						<span>车主</span>
+					</div>
+				</div>
+				<div className="person-content">
+						<div className="person-content-nav">
+							<div>
+								<img src={youhuquan_icon} alt=""/>
+								<div>优惠券</div>
+							</div>
+							<div>
+								<img src={order_icon} alt=""/>
+								<div>我的订单</div>
+							</div>
+							<div>
+								<img src={shoucang_icon} alt=""/>
+								<div>收藏车场</div>
+							</div>
+							<div>
+								<img src={parking_icon} alt=""/>
+								<div>我的车辆</div>
+							</div>
+						</div>
+						<div className="list-box">
+							<List className="my-list">
+								<Item
+									arrow="horizontal"
+									thumb={set_icon}
+									multipleLine
+									onClick={() => {}}
+								>
+									个人设置
+								</Item>
+								<Item
+									arrow="horizontal"
+									thumb={suggest_icon}
+									multipleLine
+									onClick={() => {}}
+								>
+									意见反馈
+								</Item>
+							</List>
+						</div>
+				</div>
+			</div>
+		)
+	}
 }
